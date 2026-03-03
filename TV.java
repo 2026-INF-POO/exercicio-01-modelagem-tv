38951d6043672c49eb64bee4c75187e13bf935c4
Sara <carelloamelie@gmail.com>
2026-03-03 11:47:26 -0300

    SaraTvPO
    Added attributes and methods for TV functionality.

--- TV.java
+++ TV.java
@@ -1,4 +1,31 @@
 class TV{
+  int telapolegadas;
+  string canal;
+  string marca;
+  int voltagem;
+  int volume;
+
+  void ligar() {
+
+      }
   
+   void desligar() {
+    
+  }
+
+   void aumentar() {
+    
+  }
+   void abaixar() {
+    
+  }
+
+   void subir() {
+    
+  }
+
+   void descer() {
+    
+  }
 }
 
