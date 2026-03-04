2b8aa73abad20f53807a15b3b6a07a72e7d88cc2
leandro-costa <leandro.costa@ifba.edu.br>
2026-03-04 09:13:26 -0300

    Create TV class with volume and power methods
    Add TV class with volume control and power consumption calculation.

--- TV.java
+++ TV.java
@@ -1,1 +1,13 @@
-
+class TV{
+  int volume;
+  int voltagem;
+  int polegadas;
+  int aumentarVolume(){
+    volume++;
+  }
+  //ligar: ao ligar a televisão deve imprimir seu consumo. O consumo deve ser definido pela voltagem multiplicada pela quantidades de polegadas
+  int ligar(){
+    return voltagem*polegadas;
+  }
+  
+}
